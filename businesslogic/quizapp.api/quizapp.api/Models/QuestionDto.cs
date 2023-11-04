4dd716a27c5658cf546d3b0c41dd47236275c0f7
sampletestapp <145577472+sampletestapp@users.noreply.github.com>
2023-11-05 00:23:32 +0530

    Ci questiondto update

--- businesslogic/quizapp.api/quizapp.api/Models/QuestionDto.cs
+++ businesslogic/quizapp.api/quizapp.api/Models/QuestionDto.cs
@@ -6,8 +6,8 @@
         public string QuestionNumber { get; set; }
         public string Text { get; set; }
         public int QuestionSeverityId { get; set; }
-        public string Section { get; set; }
-        public string Zones { get; set; }
+        public int SectionId { get; set; }
+        public int ZoneId { get; set; }
         public int QuestionTypeId { get; set; }
         public List<AnswerDto> Answers { get; set; }
         
